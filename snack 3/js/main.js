e1b23bd7d85d5c51806851588d149d162ef18036
Marco Rinaldi <marcorinaldi98@libero.it>
2023-03-10 15:42:14 +0100

    Snack 3 completato

--- snack 3/js/main.js
+++ snack 3/js/main.js
@@ -1,3 +1,18 @@
 /*
     JAVASCRIPT Snack 3
 */
+
+let citta = ['Roma', 'Milano', 'Torino', 'Firenze', 'Napoli', 'Palermo', 'Bologna', 'Bari'];
+
+let cittaComprese = [];
+
+visualizzaCompresi(2, 5, citta);
+
+function visualizzaCompresi(min, max, array) {
+    array.forEach((element, i) => {
+        if (i >= min && i <= max) {
+            cittaComprese.push(element);
+        }
+    });
+}
+console.log(cittaComprese);
