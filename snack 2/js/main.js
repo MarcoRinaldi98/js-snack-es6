eb58f9ab044785f2ada2ca36e90cc2242dfd8c62
Marco Rinaldi <marcorinaldi98@libero.it>
2023-03-10 15:25:34 +0100

    Snack 2 completo

--- snack 2/js/main.js
+++ snack 2/js/main.js
@@ -2,3 +2,50 @@
     JAVASCRIPT Snack 2
 */
 
+const squadre = [
+    {
+        nome: 'Roma',
+        'punti fatti': 0,
+        'falli subiti': 0
+    },
+    {
+        nome: 'Lazio',
+        'punti fatti': 0,
+        'falli subiti': 0
+    },
+    {
+        nome: 'Inter',
+        'punti fatti': 0,
+        'falli subiti': 0
+    },
+    {
+        nome: 'Milan',
+        'punti fatti': 0,
+        'falli subiti': 0
+    },
+    {
+        nome: 'Juventus',
+        'punti fatti': 0,
+        'falli subiti': 0
+    }
+];
+const containerDom = document.getElementById('container');
+
+console.log(squadre);
+let arrayConNomeEFalli = [];
+
+squadre.forEach(element => {
+    element['punti fatti'] = Math.floor(Math.random() * 50) + 1;
+    element['falli subiti'] = Math.floor(Math.random() * 20) + 1;
+
+    const {nome, 'falli subiti':falli} = element;
+    console.log(nome, falli);
+    arrayConNomeEFalli.push({nome, falli}); 
+
+    containerDom.innerHTML += ` La squadra ${nome} ha subito ${falli} falli! <br />`;
+});
+
+console.log(arrayConNomeEFalli);
+
+
+
